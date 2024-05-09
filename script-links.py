167ebadde44994a5256a73f0d7eedd000ab4fe83
Rafael <dalacquar@gmail.com>
2024-05-09 15:57:34 -0300

    Commitando mudanças

--- script-links.py
+++ script-links.py
@@ -8,17 +8,38 @@ new_data = {}
 
 # Iterar sobre as rodadas
 cont_rodada = 1
-for stage in range(1, 3):
-    for rodada in range(1, 16):
-        rodada_key = f"rodada-{str(cont_rodada).zfill(2)}"
-        rodada_data = {}
+for stage in range(1, 7):
+    if (stage <= 2):
+        for rodada in range(1, 16):
+            rodada_key = f"rodada-{str(cont_rodada).zfill(2)}"
+            rodada_data = {}
 
-        for key, value in data.items():
-            modified_link = value.replace("stage%5B%5D=1&round%5B%5D=1", f"stage%5B%5D={stage}&round%5B%5D={(rodada)}")
-            rodada_data[key] = modified_link
+            for key, value in data.items():
+                modified_link = value.replace("stage%5B%5D=1&round%5B%5D=1", f"stage%5B%5D={stage}&round%5B%5D={(rodada)}")
+                rodada_data[key] = modified_link
 
-            new_data[rodada_key] = rodada_data
-        cont_rodada += 1
+                new_data[rodada_key] = rodada_data
+            cont_rodada += 1
+    else :
+        cont_rodada = 1
+        for rodada in range(1, 6):
+            if stage == 3:
+                rodada_key = f"oitavas-{str(cont_rodada).zfill(2)}"
+            elif stage == 4:
+                rodada_key = f"quartas-{str(cont_rodada).zfill(2)}"
+            elif stage == 5:
+                rodada_key = f"semifinais-{str(cont_rodada).zfill(2)}"
+            elif stage == 6:
+                rodada_key = f"finais-{str(cont_rodada).zfill(2)}"
+
+            rodada_data = {}
+
+            for key, value in data.items():
+                modified_link = value.replace("stage%5B%5D=1&round%5B%5D=1", f"stage%5B%5D={stage}&round%5B%5D={(rodada)}")
+                rodada_data[key] = modified_link
+
+                new_data[rodada_key] = rodada_data
+            cont_rodada += 1
 
 # Salvar os novos links no arquivo JSON
 with open('./links/links-prontos/2012-2013-links.json', 'w') as file:
